fd4023587476cb886ff6cb1a9705202c4324e57c
jzb <rucjzb@163.com>
2018-06-18 19:28:17 +0800

    add RNN

--- rri.py
+++ rri.py
@@ -1,13 +1,30 @@
-import logging, time, sys
-from sklearn.base import BaseEstimator, ClassifierMixin
-import numpy as np
+import sys
 import tensorflow as tf
 from tensorflow.python.ops import variable_scope as vs
 jumper = tf.load_op_library('./jumper.so')
 
 
+def batch_slice(batch, start, offset, pad_values=None):
+    bs = tf.shape(batch)[0]
+    max_offset = tf.reduce_max(offset)
+    min_last = tf.reduce_min(tf.shape(batch)[1] - start)
+    pad_len = tf.reduce_max([max_offset - min_last, 0])
+    rank = len(batch.get_shape())
+    remain = tf.shape(batch)[2:]
+    # padding
+    batch_pad = tf.pad(batch, [[0, 0], [0, pad_len]] + [[0, 0] for r in range(rank - 2)], 'CONSTANT',
+                       constant_values=pad_values)
+    dim_len = tf.shape(batch_pad)[1]
+    # gather
+    ind_center = start + tf.range(bs) * dim_len
+    ind_region = tf.reshape(tf.expand_dims(ind_center, axis=-1) + tf.expand_dims(tf.range(max_offset), axis=0), [-1])
+    region = tf.reshape(tf.gather(tf.reshape(batch_pad, tf.concat([[-1], remain], axis=0)), ind_region),
+                        tf.concat([[bs, max_offset], remain], axis=0))
+    return region
+
+
 def rri(query, doc, dq_size, max_jump_step, word_vector, interaction='dot', glimpse='fix_hard', glimpse_fix_size=None,
-        min_density=None, jump='max_hard', represent='sum_hard'):
+        min_density=None, jump='max_hard', represent='sum_hard', rnn_size=None, max_jump_offset=None):
     bs = tf.shape(query)[0]
     max_q_len = tf.shape(query)[1]
     max_d_len = tf.shape(doc)[1]
@@ -16,7 +33,6 @@ def rri(query, doc, dq_size, max_jump_step, word_vector, interaction='dot', glim
         doc_emb = tf.nn.embedding_lookup(word_vector, doc)
     with vs.variable_scope('Match'):
         # match_matrix is of shape (batch_size, max_d_len, max_q_len)
-
         if interaction == 'indicator':
             match_matrix = tf.cast(tf.equal(tf.expand_dims(doc, axis=2), tf.expand_dims(query, axis=1)),
                                    dtype=tf.float32)
@@ -28,23 +44,13 @@ def rri(query, doc, dq_size, max_jump_step, word_vector, interaction='dot', glim
                 match_matrix /= tf.expand_dims(tf.sqrt(tf.reduce_sum(doc_emb * doc_emb, axis=2)), axis=2) * \
                                 tf.expand_dims(tf.sqrt(tf.reduce_sum(query_emb * query_emb, axis=2)), axis=1)
     with vs.variable_scope('SelectiveJump'):
-        '''
-        if jump.find('max') != -1:
-            # glimpse by looking forward 'glimpse_fix_size' cells
-            match_matrix_pad = \
-                tf.pad(match_matrix, tf.constant([[0, 0], [0, glimpse_fix_size], [0, 0]]), 'CONSTANT',
-                       constant_values=sys.float_info.min)
-        else:
-            match_matrix_pad = match_matrix
-        '''
-        match_matrix_pad = match_matrix # no need to pad in advance
         # max size of location_ta and state_ta is max_jump_step + 1
         location_ta = tf.TensorArray(dtype=tf.float32, size=1, name='location_ta',
                                      clear_after_read=False, dynamic_size=True) # (d_ind,q_ind,d_len,q_len)
         location_ta = location_ta.write(0, tf.zeros([bs, 4])) # start from the top-left corner
         state_ta = tf.TensorArray(dtype=tf.float32, size=1, name='state_ta',
                                   clear_after_read=False, dynamic_size=True)
-        if represent == 'sum_hard' or represent == 'test':
+        if represent == 'sum_hard' or represent == 'test' or represent == 'rnn_hard':
             state_ta = state_ta.write(0, tf.zeros([bs, 1]))
         else:
             raise NotImplementedError()
@@ -54,7 +60,7 @@ def rri(query, doc, dq_size, max_jump_step, word_vector, interaction='dot', glim
         '''
         get next glimpse location (g_t+1) based on last jump location (j_t)
         '''
-        def get_glimpse_location(match_matrix_pad, dq_size, location):
+        def get_glimpse_location(match_matrix, dq_size, location):
             if glimpse == 'fix_hard':
                 gp_d_position = tf.cast(tf.floor(location[:, 0] + location[:, 2]), dtype=tf.int32)
                 gp_d_offset = tf.reduce_min([tf.ones_like(dq_size[:, 0], dtype=tf.int32) * glimpse_fix_size,
@@ -76,11 +82,11 @@ def rri(query, doc, dq_size, max_jump_step, word_vector, interaction='dot', glim
         '''
         get next jump location (j_t+1) based on glimpse location (g_t+1)
         '''
-        def get_jump_location(match_matrix_pad, dq_size, location):
+        def get_jump_location(match_matrix, dq_size, location):
             if jump == 'max_hard':
                 max_d_offset = tf.cast(tf.floor(tf.reduce_max(location[:, 2])), dtype=tf.int32)
                 # padding
-                match_matrix_pad = tf.pad(match_matrix_pad, [[0, 0], [0, max_d_offset], [0, 0]], 'CONSTANT',
+                match_matrix_pad = tf.pad(match_matrix, [[0, 0], [0, max_d_offset], [0, 0]], 'CONSTANT',
                                           constant_values=sys.float_info.min)
                 d_len = tf.shape(match_matrix_pad)[1]
                 start = tf.cast(tf.floor(location[:, 0]), dtype=tf.int32)
@@ -93,11 +99,12 @@ def rri(query, doc, dq_size, max_jump_step, word_vector, interaction='dot', glim
                 new_location = tf.stack([tf.cast(d_loc, dtype=tf.float32),
                                          location[:, 1], tf.ones([bs]), location[:, 3]], axis=1)
             elif jump == 'min_density_hard':
-                #new_location = jumper.min_density(match_matrix=match_matrix_pad, dq_size=dq_size, location=location,
+                #new_location = jumper.min_density(match_matrix=match_matrix, dq_size=dq_size, location=location,
                 #                                  min_density=min_density)
                 # there is no need to use multi-thread op, because this is fast and thus not the bottleneck
                 new_location = jumper.min_density_multi_cpu(
-                    match_matrix=match_matrix_pad, dq_size=dq_size, location=location, min_density=min_density)
+                    match_matrix=match_matrix, dq_size=dq_size, location=location, min_density=min_density)
+                new_location = tf.stop_gradient(new_location)
             elif jump == 'test':
                 new_location = location[:, 0] + tf.reduce_min([tf.ones_like(location[:, 1]), location[:, 1]])
                 new_location = tf.stack([new_location, location[:, 1], tf.ones([bs]), location[:, 3]], axis=1)
@@ -107,7 +114,7 @@ def rri(query, doc, dq_size, max_jump_step, word_vector, interaction='dot', glim
         '''
         get the representation based on location (j_t+1)
         '''
-        def get_representation(match_matrix_pad, dq_size, query_emb, doc_emb, location):
+        def get_representation(match_matrix, dq_size, query_emb, doc_emb, location):
             if represent == 'sum_hard':
                 start = tf.cast(tf.floor(location[:, :2]), dtype=tf.int32)
                 end = tf.cast(tf.floor(location[:, :2] + location[:, 2:]), dtype=tf.int32)
@@ -120,9 +127,23 @@ def rri(query, doc, dq_size, max_jump_step, word_vector, interaction='dot', glim
                     return i + 1, m, s, e, r
                 _, _, _, _, representation_ta = \
                     tf.while_loop(lambda i, m, s, e, r: i < bs, body,
-                                  [ind, match_matrix_pad, start, end, representation_ta],
+                                  [ind, match_matrix, start, end, representation_ta],
                                   parallel_iterations=1000)
                 representation = representation_ta.stack()
+            elif represent == 'rnn_hard':
+                start = tf.cast(tf.floor(location[:, :2]), dtype=tf.int32)
+                offset = tf.cast(tf.floor(location[:, 2:]), dtype=tf.int32)
+                d_start, d_offset = start[:, 0], offset[:, 0]
+                q_start, q_offset = start[:, 1], offset[:, 1]
+                d_region = batch_slice(doc_emb, d_start, d_offset, pad_values=0)
+                q_region = batch_slice(query_emb, q_start, q_offset, pad_values=0)
+                rnn_cell = tf.nn.rnn_cell.BasicRNNCell(rnn_size)
+                initial_state = rnn_cell.zero_state(bs, dtype=tf.float32)
+                d_outputs, d_state = tf.nn.dynamic_rnn(rnn_cell, d_region, initial_state=initial_state,
+                                                       sequence_length=d_offset, dtype=tf.float32)
+                q_outputs, q_state = tf.nn.dynamic_rnn(rnn_cell, q_region, initial_state=initial_state,
+                                                       sequence_length=q_offset, dtype=tf.float32)
+                representation = tf.reduce_sum(d_state * q_state, axis=1, keep_dims=True)
             elif represent == 'test':
                 representation = tf.ones_like(location[:, :1])
             else:
@@ -133,20 +154,26 @@ def rri(query, doc, dq_size, max_jump_step, word_vector, interaction='dot', glim
                                   tf.less(time, tf.constant(max_jump_step)))
         def body(time, is_stop, step, state_ta, location_ta, dq_size):
             cur_location = location_ta.read(time)
+            #time = tf.Print(time, [time], message='time:')
             with vs.variable_scope('Glimpse'):
-                glimpse_location = get_glimpse_location(match_matrix_pad, dq_size, cur_location)
+                glimpse_location = get_glimpse_location(match_matrix, dq_size, cur_location)
                 # stop when the start index overflow
                 new_stop = tf.reduce_any(glimpse_location[:, :2] > tf.cast(dq_size - 1, tf.float32), axis=1)
                 glimpse_location = tf.where(new_stop, cur_location, glimpse_location)
                 is_stop = tf.logical_or(is_stop, new_stop)
             with vs.variable_scope('Jump'):
-                new_location = get_jump_location(match_matrix_pad, dq_size, glimpse_location)
+                new_location = get_jump_location(match_matrix, dq_size, glimpse_location)
+                if max_jump_offset != None:
+                    new_location = tf.concat([new_location[:, :2],
+                                              tf.minimum(new_location[:, 2:], max_jump_offset)], axis=1)
                 # stop when the start index overflow
                 new_stop = tf.reduce_any(new_location[:, :2] > tf.cast(dq_size - 1, tf.float32), axis=1)
                 is_stop = tf.logical_or(is_stop, new_stop)
                 location_ta = location_ta.write(time + 1, tf.where(is_stop, cur_location, new_location))
             with vs.variable_scope('Represent'):
-                new_repr = get_representation(match_matrix_pad, dq_size, query_emb, doc_emb, location_ta.read(time + 1))
+                # location_one_out is to prevent duplicate time-consuming calculation
+                location_one_out = tf.where(is_stop, tf.ones_like(cur_location), location_ta.read(time + 1))
+                new_repr = get_representation(match_matrix, dq_size, query_emb, doc_emb, location_one_out)
                 state_ta = state_ta.write(time + 1, tf.where(is_stop, state_ta.read(time), new_repr))
             step = step + tf.where(is_stop, tf.zeros([bs], dtype=tf.int32), tf.ones([bs], dtype=tf.int32))
             return time + 1, is_stop, step, state_ta, location_ta, dq_size
@@ -156,4 +183,10 @@ def rri(query, doc, dq_size, max_jump_step, word_vector, interaction='dot', glim
         states = state_ta.stack()
         location = location_ta.stack()
         location = tf.transpose(location, [1, 0 ,2])
-        return tf.reduce_max(states, 0), {'step': step, 'location': location, 'match_matrix': match_matrix}
+        stop_ratio = tf.reduce_mean(tf.cast(is_stop, tf.float32))
+        complete_ratio = tf.reduce_mean(tf.reduce_min(
+            [(location[:, -1, 0] + location[:, -1, 2]) / tf.cast(dq_size[:, 1], dtype=tf.float32),
+             tf.ones([bs], dtype=tf.float32)], axis=0))
+        return tf.reduce_max(states, 0), {'step': step, 'location': location, 'match_matrix': match_matrix,
+                                          'complete_ratio': complete_ratio, 'stop_ratio': stop_ratio,
+                                          'doc_emb': doc_emb}
--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import argparse, logging, os, random, time
 from itertools import groupby
 import numpy as np
+from sklearn.base import BaseEstimator, ClassifierMixin
 import tensorflow as tf
 from tensorflow.python.ops import variable_scope as vs
 from utils import Vocab, WordVector, load_prep_file, load_train_test_file, printoptions
@@ -27,7 +28,8 @@ def data_pad(samples, max_len, dtype=None):
     return np.array([s + [0] * (max_len - len(s)) for s in samples], dtype=dtype)
 
 
-def data_assemble(filepath, query_raw, doc_raw, max_q_len, max_d_len):
+def data_assemble(filepath, query_raw, doc_raw, max_q_len, max_d_len, relevance_mapper=None):
+    relevance_mapper = relevance_mapper or (lambda x: x)
     samples = load_train_test_file(filepath)
     samples_gb_q = groupby(samples, lambda x:x[0])
     X = []
@@ -36,6 +38,8 @@ def data_assemble(filepath, query_raw, doc_raw, max_q_len, max_d_len):
         def batcher(X, y, batch_size):
             rb = batch_size
             result = {
+                'qid': [],
+                'docid': [],
                 'qd_size': [],
                 'relevance': [],
                 'query': [],
@@ -49,6 +53,8 @@ def data_assemble(filepath, query_raw, doc_raw, max_q_len, max_d_len):
                 remain_n_sample = len(q_x['query']) - doc_ind
                 take_n_sample = min(remain_n_sample, rb)
                 for d in range(doc_ind, doc_ind + take_n_sample):
+                    result['qid'].append(q_x['qid'][d])
+                    result['docid'].append(q_x['docid'][d])
                     result['qd_size'].append(q_x['qd_size'][d])
                     result['relevance'].append(q_y['relevance'][d])
                     result['query'].append(q_x['query'][d])
@@ -67,9 +73,13 @@ def data_assemble(filepath, query_raw, doc_raw, max_q_len, max_d_len):
                     yield_result['query'] = data_pad(result['query'], np.max(yield_result['qd_size'][:, 0]),
                                                np.int32)
                     yield_result['doc'] = data_pad(result['doc'], np.max(yield_result['qd_size'][:, 1]), np.int32)
+                    #print('qid: {}'.format(list(zip(range(len(result['qid'])), result['qid']))))
+                    #print('docid: {}'.format(list(zip(range(len(result['docid'])), result['docid']))))
                     yield yield_result
                     rb = batch_size
                     result = {
+                        'qid': [],
+                        'docid': [],
                         'qd_size': [],
                         'relevance': [],
                         'query': [],
@@ -94,7 +104,7 @@ def data_assemble(filepath, query_raw, doc_raw, max_q_len, max_d_len):
                 q_x['qd_size'].append([len(query_raw[s[0]]), len(doc_raw[s[1]])])
                 q_x['qid'].append(s[0])
                 q_x['docid'].append(s[1])
-                q_y['relevance'].append(s[2])
+                q_y['relevance'].append(relevance_mapper(s[2]))
             X.append(q_x)
             y.append(q_y)
         return X, y, batcher
@@ -105,20 +115,25 @@ def data_assemble(filepath, query_raw, doc_raw, max_q_len, max_d_len):
 class RRI(object):
     INTERACTION = {'dot', 'cosine', 'indicator'}
 
-    def __init__(self, max_q_len, max_d_len, max_jump_step, word_vector=None, interaction='dot',
-                 glimpse='fix_hard', glimpse_fix_size=None, min_density=None, jump='max_hard', represent='sum_hard', rel_level=2,
-                 learning_rate=0.1, random_seed=0, n_epochs=100, batch_size=100, batcher=None, verbose=True,
-                 save_epochs=None, reuse_model=None, save_model=None, summary_path=None):
+    def __init__(self, max_q_len, max_d_len, max_jump_step, word_vector=None, vocab=None, word_vector_trainable=True,
+                 interaction='dot', glimpse='fix_hard', glimpse_fix_size=None, min_density=None, jump='max_hard',
+                 represent='sum_hard', rnn_size=None, max_jump_offset=None, rel_level=2, learning_rate=0.1,
+                 random_seed=0, n_epochs=100, batch_size=100, batcher=None, verbose=True, save_epochs=None,
+                 reuse_model=None, save_model=None, summary_path=None):
         self.max_q_len = max_q_len
         self.max_d_len = max_d_len
         self.max_jump_step = max_jump_step
         self.word_vector = word_vector
+        self.vocab = vocab
+        self.word_vector_trainable = word_vector_trainable
         self.interaction = interaction
         self.glimpse = glimpse
         self.glimpse_fix_size = glimpse_fix_size
         self.min_density = min_density
         self.jump = jump
         self.represent = represent
+        self.rnn_size = rnn_size
+        self.max_jump_offset = max_jump_offset
         self.rel_level = rel_level
         self.learning_rate = learning_rate
         self.random_seed= random_seed
@@ -138,11 +153,15 @@ class RRI(object):
             self.doc = tf.placeholder(tf.int32, shape=[None, None], name='doc') # dynamic doc length
             self.qd_size = tf.placeholder(tf.int32, shape=[None, 2], name='query_doc_size')
             self.relevance = tf.placeholder(tf.int32, shape=[None], name='relevance')
-        self.outputs, self.rri_info = \
-            rri(self.query, self.doc, tf.reverse(self.qd_size, [1]), max_jump_step=self.max_jump_step,
-                word_vector=self.word_vector, interaction=self.interaction, glimpse=self.glimpse,
-                glimpse_fix_size=self.glimpse_fix_size, min_density=self.min_density,
-                jump=self.jump, represent=self.represent)
+            self.word_vector_variable = tf.get_variable('word_vector', self.word_vector.shape,
+                                                        initializer=tf.constant_initializer(self.word_vector),
+                                                        trainable=self.word_vector_trainable)
+        with vs.variable_scope('Arch'):
+            self.outputs, self.rri_info = \
+                rri(self.query, self.doc, tf.reverse(self.qd_size, [1]), max_jump_step=self.max_jump_step,
+                    word_vector=self.word_vector_variable, interaction=self.interaction, glimpse=self.glimpse,
+                    glimpse_fix_size=self.glimpse_fix_size, min_density=self.min_density, jump=self.jump,
+                    represent=self.represent, rnn_size=self.rnn_size, max_jump_offset=self.max_jump_offset)
         with vs.variable_scope('Loss'):
             logit_w = tf.get_variable('logits_weight', shape=[self.outputs.get_shape()[1], self.rel_level])
             logit_b = tf.get_variable('logits_bias', shape=[self.rel_level], initializer=tf.constant_initializer())
@@ -155,6 +174,9 @@ class RRI(object):
         self.saver = tf.train.Saver()
         if self.summary_path != None:
             self.train_writer = tf.summary.FileWriter(os.path.join(self.summary_path, 'train'), self.graph_)
+        self.test_wv_grad = tf.gradients(self.loss, [self.word_vector_variable])[0]
+        self.test_rnn_grad = tf.gradients(self.loss, [v for v in tf.global_variables() 
+            if 'rnn' in v.name and 'kernel' in v.name and 'Adam' not in v.name])[0]
 
 
     def check_params(self):
@@ -195,23 +217,45 @@ class RRI(object):
             for i, fd in enumerate(self.batcher(X, y, self.batch_size)):
                 batch_size = len(fd['query'])
                 fetch = [self.rri_info['step'], self.rri_info['location'], self.rri_info['match_matrix'],
-                         self.loss, self.trainer]
+                         self.loss, self.rri_info['complete_ratio'], self.rri_info['stop_ratio'], self.trainer]
                 feed_dict = {self.query: fd['query'], self.doc: fd['doc'], self.qd_size: fd['qd_size'],
                              self.relevance: fd['relevance']}
                 start_time = time.time()
                 if self.summary_path != None and i % 1 == 0: # run statistics
                     run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
                     run_metadata = tf.RunMetadata()
-                    step, location, match_matrix, loss, _ = \
+                    step, location, match_matrix, loss, com_r, stop_r, _ = \
                         self.session_.run(fetch, feed_dict=feed_dict, options=run_options, run_metadata=run_metadata)
                     self.train_writer.add_run_metadata(run_metadata, 'step%d' % i)
                     print('adding run metadata for', i)
                 else:
-                    step, location, match_matrix, loss, _ = self.session_.run(fetch, feed_dict=feed_dict)
+                    step, location, match_matrix, loss, com_r, stop_r, _ = self.session_.run(fetch, feed_dict=feed_dict)
                 loss_list.append(loss)
-                print(i, time.time() - start_time, loss,
-                      np.mean([fd['qd_size'][b, 1] / (step[b] or 1) for b in range(batch_size)]))
+                print('{:<5}\t{:>5.3f}\tloss:{:>5.3f}\tratio:{:>3.2f} {:>3.2f}'
+                    .format(i, time.time() - start_time, loss, com_r, stop_r))
+                if args.debug and hasattr(self, 'test_rnn_grad'):
+                    test_rnn_grad, = self.session_.run([self.test_rnn_grad], feed_dict=feed_dict)
+                    with printoptions(precision=3, suppress=True, threshold=np.nan):
+                        print(np.max(np.abs(test_rnn_grad), axis=1))
+                        #input()
+                if args.debug and self.word_vector_trainable and hasattr(self, 'test_wv_grad'):
+                    test_grad, = self.session_.run([self.test_wv_grad], feed_dict=feed_dict)
+                    with printoptions(precision=3, suppress=True, threshold=np.nan):
+                        print(type(test_grad))
+                        print(len(test_grad.indices))
+                        #print(np.sum(np.any(test_grad[0] != 0, axis=2), axis=1))
+                        inds = np.any(np.abs(test_grad.values) >= .001, axis=1)
+                        w_inds, u_w_inds = test_grad.indices[inds], np.unique(test_grad.indices[inds])
+                        print(np.sum(w_inds), len(u_w_inds))
+                        print(w_inds)
+                        print(u_w_inds)
+                        print(self.vocab.decode(u_w_inds))
+                        print(np.max(np.abs(test_grad.values[inds]), axis=1))
+                        #input()
                 if args.debug:
+                    cont = input('continue debug? y for yes:')
+                    if cont != 'y':
+                        continue
                     with printoptions(precision=3, suppress=True, threshold=np.nan):
                         for b in range(batch_size):
                             print(fd['qd_size'][b, 1], step[b])
@@ -235,6 +279,22 @@ class RRI(object):
                 print('')
 
 
+    def test(self, X, y=None):
+        # check params
+        self.check_params()
+        if not hasattr(self, 'session_'):
+            raise AttributeError('need fit or fit_iterable to be called before prediction')
+        loss_list = []
+        for i, fd in enumerate(self.batcher(X, y, self.batch_size)):
+            if i >= 10:
+                continue
+            feed_dict = {self.query: fd['query'], self.doc: fd['doc'], self.qd_size: fd['qd_size'], 
+                         self.relevance: fd['relevance']}
+            loss, = self.session_.run([self.loss], feed_dict=feed_dict)
+            loss_list.append(loss)
+        return np.mean(loss_list)
+
+
 def train_test():
     train_file = os.path.join(args.data_dir, 'train.prep.pointwise')
     test_file = os.path.join(args.data_dir, 'test.prep.pointwise')
@@ -242,6 +302,7 @@ def train_test():
     doc_file = os.path.join(args.data_dir, 'docs.prep')
     w2v_file = os.path.join(args.data_dir, 'w2v')
     vocab_file = os.path.join(args.data_dir, 'vocab')
+    rel_level = 2
     print('loading word vector ...')
     wv = WordVector(filepath=w2v_file)
     vocab = Vocab(filepath=vocab_file)
@@ -251,14 +312,25 @@ def train_test():
     max_q_len = max([len(query_raw[q]) for q in query_raw])
     max_d_len = max([len(doc_raw[d]) for d in doc_raw])
     print('data assemble with max_q_len: {}, max_d_len: {} ...'.format(max_q_len, max_d_len))
-    train_X, train_y, batcher = data_assemble(train_file, query_raw, doc_raw, max_q_len, max_d_len)
+    def relevance_mapper(r):
+        if r < 0:
+            return 0
+        if r >= rel_level:
+            return rel_level - 1
+        return r
+    train_X, train_y, batcher = data_assemble(train_file, query_raw, doc_raw, max_q_len, max_d_len, 
+                                              relevance_mapper=relevance_mapper)
+    test_X, test_y, _ = data_assemble(test_file, query_raw, doc_raw, max_q_len, max_d_len, 
+                                      relevance_mapper=relevance_mapper)
     print('number of samples: {}'.format(sum([len(x['query']) for x in train_X])))
-    rri = RRI(max_q_len=max_q_len, max_d_len=max_d_len, max_jump_step=100, word_vector=wv.get_vectors(normalize=True),
-              interaction='dot', glimpse='all_next', glimpse_fix_size=10, min_density=0.5,
-              jump='min_density_hard', represent='sum_hard', rel_level=5, learning_rate=0.1, random_seed=SEED,
-              n_epochs=10, batch_size=1000, batcher=batcher, verbose=True, save_epochs=None,
-              reuse_model=None, save_model=None, summary_path=args.tf_summary_path)
-    list(rri.fit_iterable(train_X, train_y))
+    rri = RRI(max_q_len=max_q_len, max_d_len=max_d_len, max_jump_step=500, word_vector=wv.get_vectors(normalize=True),
+              vocab=vocab, word_vector_trainable=False, interaction='dot', glimpse='all_next', glimpse_fix_size=10,
+              min_density=0.5, jump='min_density_hard', represent='rnn_hard', rnn_size=100, max_jump_offset=100,
+              rel_level=rel_level, learning_rate=0.005, random_seed=SEED, n_epochs=10, batch_size=100, batcher=batcher,
+              verbose=True, save_epochs=1, reuse_model=None, save_model=None, summary_path=args.tf_summary_path)
+    for e in rri.fit_iterable(train_X, train_y):
+        loss = rri.test(test_X, test_y)
+        print('\t{:>7}:{:>5.3f}'.format('test', loss), end='', flush=True)
 
 
 if __name__ == '__main__':
